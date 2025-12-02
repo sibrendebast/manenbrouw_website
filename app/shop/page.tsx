5896d3e8f40b6f7e4b78966843636027dbfed5bc
Sibren De Bast <debastsibren@gmail.com>
2025-12-02 21:39:45 +0100

    Fix implicit any type error in shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -17,7 +17,7 @@ export default async function ShopPage() {
                 </div>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {products.map((product) => (
+                    {products.map((product: any) => (
                         <ProductCard key={product.id} product={product} />
                     ))}
                 </div>
